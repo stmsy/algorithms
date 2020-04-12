b1aa520131339651967d60a08729d883d39c1961
Masaya Sato <sato.masaya.dev@gmail.com>
2020-04-12 11:27:30 +0900

    Add shell sort

---
+++ sort/shell/shell.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+
+int shell(int a[], int n)
+{
+    int i, j, h, tmp;
+    for (h = n/2; h > 0; h = h/2) {
+        for (i = h; i < n; i++) {
+	    tmp = a[i];
+	    for (j = i-h; j >= h-1 && a[j] > tmp; j -= h) {
+		a[j+h] = a[j];
+	    }
+	    a[j+h] = tmp;
+	}
+    }
+    return 0;
+}
---
+++ sort/shell/shell.h
@@ -0,0 +1,6 @@
+#ifndef _SHELL_H_INCLUDED_
+#define _SHELL_H_INCLUDED_
+
+int shell(int a[], int n);
+
+#endif
